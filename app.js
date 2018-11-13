d736d79cc9fdc761e792d499f7badb9b62d25916
Martin Gallegos <magallegosp@gmail.com>
2018-11-13 15:15:01 -0700

    Using templates and changing port

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 
@@ -18,6 +19,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.listen(3000, () => {
-  debug(`Listening on port ${chalk.blue(3000)}`);
+app.listen(port, () => {
+  debug(`Listening on port ${chalk.blue(port)}`);
 });
